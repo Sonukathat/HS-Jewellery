773071c87aa3d6464def4250e9bb3b00db6914f3
sonukathat <skkhankathat93@gmail.com>
2025-08-25 10:15:56 +0530

    completed category mvc

--- backend/controllers/categoryController.js
+++ backend/controllers/categoryController.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import Category from "../models/categoryModel.js";
 
 export const createCategory = async (req, res) => {
@@ -16,35 +17,83 @@ export const createCategory = async (req, res) => {
 
         res.status(200).json({ success: true })
     } catch (error) {
-         res.status(500).json({message:error.message})
+        res.status(500).json({ message: error.message })
     }
 }
 
-export const getCategory = async (req,res) =>{
+export const getCategory = async (req, res) => {
     try {
-        const {name} = req.body;
+        const { name } = req.body;
 
-        if(!name){
+        if (!name) {
             res.status(400).json({
-                success:true,
-                message:"category name is required"
+                success: true,
+                message: "category name is required"
             })
         }
 
-        const categories = await Category.find({name});
+        const categories = await Category.find({ name });
 
-        if(categories.length === 0){
+        if (categories.length === 0) {
             res.status(400).json({
-                success:false,
-                message:"NO category found with this name"
+                success: false,
+                message: "NO category found with this name"
             })
         }
 
         res.status(200).json({
-            message:true,
+            message: true,
             categories
         });
     } catch (error) {
-        res.status(500).json({message:error.message})
+        res.status(500).json({ message: error.message })
     }
+}
+
+export const updateCategory = async (req, res) => {
+    try {
+        const {id} = req.params;
+        const { name } = req.body;
+
+        if(!mongoose.Types.ObjectId.isValid(id)){
+            return res.status(400).json({
+                success:false,
+                message:"Invalid id formate"
+            })
+        }
+
+        const updated = await Category.findByIdAndUpdate(id,{ name },{new:true});
+        
+        if(!updated){
+            return res.status(400).json({
+                success:false,
+                message:"id not found"
+            })
+        }
+
+        res.status(200).json({
+            success: true,
+            updated
+        })
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+export const deleteCategory = async (req,res)=>{
+    const {id} = req.params;
+
+    const deleted = await Category.findByIdAndDelete(id);
+
+    if(!deleted){
+        return res.status(200).json({
+            success:true,
+            message:"item not found"
+        })
+    }
+
+    res.status(200).json({
+        success:true,
+        message:"item deleted"
+    })
 }
--- backend/routes/categoryRoute.js
+++ backend/routes/categoryRoute.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import { createCategory, getCategory } from '../controllers/categoryController.js';
+import { createCategory, deleteCategory, getCategory, updateCategory } from '../controllers/categoryController.js';
 
 
 const router = express.Router();
 
 router.post('/add',createCategory);
-router.get('/getcategory',getCategory);
+router.get('/get',getCategory);
+router.put('/update/:id',updateCategory);
+router.delete('/delete/:id',deleteCategory);
 
 export default router;
