f7fb5ef9adc9018bc416ce1eae78b104bee07762
sonukathat <skkhankathat93@gmail.com>
2025-09-12 16:41:37 +0530

    added jumka images

--- frontend/src/components/Jhumkas.jsx
+++ frontend/src/components/Jhumkas.jsx
@@ -1,18 +1,26 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 
 function Jhumkas() {
 
-    const ImageUrls = [
-        "https://images.unsplash.com/photo-1616586169190-fb7ff2abd757?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Z2lybCUyMGVhciUyMGpodW1rYXMlMjBqZXdlbGVyeSUyMHBpY3MlMjBoZHxlbnwwfHwwfHx8MA%3D%3D",
-        "https://images.unsplash.com/photo-1625516152414-8f33eef3d660?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Z2lybCUyMGVhciUyMGpodW1rYXMlMjBqZXdlbGVyeSUyMHBpY3MlMjBoZHxlbnwwfHwwfHx8MA%3D%3D",
-        "https://plus.unsplash.com/premium_photo-1691030255383-ec9765ad5340?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Z2lybCUyMGVhciUyMGpodW1rYXMlMjBqZXdlbGVyeSUyMHBpY3MlMjBoZHxlbnwwfHwwfHx8MA%3D%3D",
-        "https://images.unsplash.com/photo-1652766540048-de0a878a3266?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Z2lybCUyMGVhciUyMGpodW1rYXMlMjBqZXdlbGVyeSUyMHBpY3MlMjBoZHxlbnwwfHwwfHx8MA%3D%3D",
-        "https://www.machki.com/cdn/shop/files/IMG_6286_5108f6f4-32e2-4f9e-8e01-7eda6ea73f86.heic?v=1726898436&width=360",
-        "https://plus.unsplash.com/premium_photo-1675107359599-a2d0d8983c36?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGdpcmwlMjBlYXIlMjBqaHVta2FzJTIwamV3ZWxlcnklMjBwaWNzJTIwaGR8ZW58MHx8MHx8fDA%3D",
-        "https://images.unsplash.com/photo-1535632066927-ab7c9ab60908?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8ODN8fGdpcmwlMjBlYXIlMjBqaHVta2FzJTIwamV3ZWxlcnklMjBwaWNzJTIwaGR8ZW58MHx8MHx8fDA%3D",
-        "https://www.machki.com/cdn/shop/files/IMG_6440.heic?v=1726897372&width=360"
-    ];
+    const[jumka,setJumka]=useState([])
 
+    useEffect(()=>{
+        const fetchJumka = async ()=>{
+            try {
+                const res = await axios.get('http://localhost:5000/category/get');
+                const selected = res.data.categories[3].images;
+
+                // console.log(selected);
+                
+                setJumka(selected);
+            } catch (error) {
+                console.error("Error fetching images:", error);
+            }
+        }
+        fetchJumka();
+    },[])
 
     return (
         <div className='flex flex-col gap-4'>
@@ -20,7 +28,7 @@ function Jhumkas() {
                 <h2 className='text-4xl ml-4 py-10'>Jhumkas</h2>
             </div>
             <div className='grid grid-cols-1 gap-8 px-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
-                {ImageUrls.map((src, i) => (
+                {jumka.slice(0,8).map((src, i) => (
                     <div key={i} className='border border-gray-200 rounded-md hover:border-gray-500 transition-all duration-300 ease-in-out overflow-hidden'>
                         <img src={src} alt="jewe" className='h-96 w-full rounded-t-md transition-transform duration-500 ease-in-out transform hover:scale-101' />
                         <button className='bg-green-950 text-white px-3 py-1 rounded-md relative left-2 bottom-8 text-sm'>Sale</button>
