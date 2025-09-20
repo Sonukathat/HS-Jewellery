92dbf1b11f2fdcb96db3bd0c483e17730c03973f
sonukathat <skkhankathat93@gmail.com>
2025-09-20 20:34:18 +0530

    added in cart

--- frontend/src/components/Emptycart.jsx
+++ frontend/src/components/Emptycart.jsx
@@ -1,19 +1,25 @@
 import { Link } from "react-router-dom"
+import Header from "./Header"
+import Footer from "./Footer"
 
 function Emptycart() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen gap-8">
-      <div>
-        <h2 className="text-3xl">Your cart is empty</h2>
+    <>
+      <Header />
+      <div className="flex flex-col items-center justify-center h-screen gap-8">
+        <div>
+          <h2 className="text-3xl">Your cart is empty</h2>
+        </div>
+        <div>
+          <button className="bg-gray-800 px-8 py-3 text-white rounded-md">Continue shopping</button>
+        </div>
+        <div>
+          <h2 className="text-xl">Have an account?</h2>
+          <p className="text-md"><Link to="/signin" className="border-b hover:border-b-3">Log in</Link> to check out faster.</p>
+        </div>
       </div>
-      <div>
-        <button className="bg-gray-800 px-8 py-3 text-white rounded-md">Continue shopping</button>
-      </div>
-      <div>
-        <h2 className="text-xl">Have an account?</h2>
-        <p className="text-md"><Link to="/signin" className="border-b hover:border-b-3">Log in</Link> to check out faster.</p>
-      </div>
-    </div>
+      <Footer />
+    </>
   )
 }
 
