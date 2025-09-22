808a0991de7aa4da8918abeeb7c2e83511e73933
sonukathat <skkhankathat93@gmail.com>
2025-09-22 19:56:35 +0530

    update jhumkas compon.

--- frontend/src/components/Jhumkas.jsx
+++ frontend/src/components/Jhumkas.jsx
@@ -35,7 +35,7 @@ function Jhumkas() {
             </div>
             <div className='grid grid-cols-1 gap-8 px-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
                 {jumka.slice(0, 8).map((src, i) => (
-                    <div key={i} className='border border-gray-200 rounded-md hover:border-gray-500 transition-all duration-300 ease-in-out overflow-hidden'>
+                    <div key={i} className='cursor-pointer border border-gray-200 rounded-md hover:border-gray-500 transition-all duration-300 ease-in-out overflow-hidden'>
                         <img src={src.image} alt="jewe" className='h-96 w-full rounded-t-md transition-transform duration-500 ease-in-out transform hover:scale-101' />
                         <button className='bg-green-950 text-white px-3 py-1 rounded-md relative left-2 bottom-8 text-sm'>Sale</button>
                         <p className='mt-2 ml-5 font-serif'>{src.name}</p>
@@ -48,7 +48,7 @@ function Jhumkas() {
                 }
             </div>
             <div className='flex justify-center py-8'>
-                <button className='bg-green-950 text-white px-8 py-3 rounded-md'>View all</button>
+                <button className='bg-green-950 text-white px-8 py-3 rounded-md cursor-pointer'>View all</button>
             </div>
         </div>
     )
