b05064cd64031e81026ad968f4d135af988c4baf
sonukathat <skkhankathat93@gmail.com>
2025-09-25 12:26:08 +0530

    admin route in backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -61,7 +61,7 @@ export const loginUser = async (req, res) => {
         res.status(200).json({
             success: true,
             token : token,
-            user:user.isAdmin
+            user:user
         })
     } catch (error) {
         res.status(500).json({ message: error.message })
