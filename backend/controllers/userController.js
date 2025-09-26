f00ed127a1d980537fb030acf5acf7bef3595383
sonukathat <skkhankathat93@gmail.com>
2025-09-26 16:45:06 +0530

    increase jwt token value

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -55,7 +55,7 @@ export const loginUser = async (req, res) => {
         const token = jwt.sign(
             { id: user._id, isAdmin: user.isAdmin },
             process.env.JWT_SECRET,
-            { expiresIn: "1h" }
+            { expiresIn: "7d" }
         );
 
         res.status(200).json({
