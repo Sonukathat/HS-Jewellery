7cfcdd3fa7c6d34516cdb5e5389be19e37b09159
sonukathat <skkhankathat93@gmail.com>
2025-09-27 12:52:13 +0530

    fetch user data

--- frontend/src/components/Userprofile.jsx
+++ frontend/src/components/Userprofile.jsx
@@ -1,44 +1,44 @@
-import { useEffect, useState } from "react";
 import axios from "axios";
+import { useEffect, useState } from "react";
 
 const Userprofile = () => {
-  const [user, setUser] = useState(null); // user data
-  const [loading, setLoading] = useState(true); 
-  const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const fetchUserProfile = async () => {
-      try {
-        const token = localStorage.getItem("token"); // frontend me token save hota hai login ke baad
-        const res = await axios.get("http://localhost:5000/users/profile", {
-          headers: {
-            Authorization: `Bearer ${token}`, // token header me bhejna zaroori hai
-          },
-        });
+    const[user,setUser]=useState({});
 
-        setUser(res.data.data); // backend se aaya user data
-        setLoading(false);
-      } catch (err) {
-        setError(err.response?.data?.message || "Something went wrong");
-        setLoading(false);
-      }
-    };
+    useEffect(() => {
+        const user = async () => {
+            try {
 
-    fetchUserProfile();
-  }, []);
+                const token = localStorage.getItem("token");
+                const res = await axios.get('http://localhost:5000/users/profile',{
+                    headers:{
+                        Authorization: `Bearer ${token}`
+                    }
+                })
+                //  console.log(res.data.data);
+                setUser(res.data.data);
+            } catch (error) {
+                console.log("error", error);
+            }
+        }
+        user();
+    }, [])
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p className="text-red-500">{error}</p>;
+    let name = "sonukathat";
+    let email = "sonu@FaGlassMartiniAlt.com";
 
-  return (
-    <div className="flex flex-col items-center mt-10">
-      <div className="w-32 h-32 bg-gray-200 rounded-full flex items-center justify-center text-gray-500 text-5xl font-bold mb-4">
-        {user.name.charAt(0)}
-      </div>
-      <h2 className="text-2xl font-bold">{user.name}</h2>
-      <p className="text-gray-600">{user.email}</p>
-    </div>
-  );
+    return (
+        <div className="flex flex-col items-center bg-gradient-to-r from-white via-[#f8dcdb] to-white p-6 rounded-2xl shadow-lg max-w-sm mx-auto my-10">
+            <div className="w-32 h-32 bg-white rounded-full flex items-center justify-center text-gray-400 text-6xl font-bold mb-4 shadow-inner">
+                {name.charAt(0)}
+            </div>
+
+            <h2 className="text-2xl font-bold text-black mb-2">{user.name}</h2>
+
+            <p className="text-black text-sm opacity-80">{user.email}</p>
+
+        </div>
+    );
 };
 
 export default Userprofile;
