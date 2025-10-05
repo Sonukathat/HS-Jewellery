76eee7144179116d4801cd4f18e5f16de3e666fe
sonukathat <skkhankathat93@gmail.com>
2025-10-05 14:34:45 +0530

    changes in upload middleware

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,37 +1,17 @@
-import multer from "multer";
-import path from "path";
-import fs from "fs";
+import multer from 'multer';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import cloudinary from '../config/cloudinary.js'; // Cloudinary config file
 
-const UPLOADS_FOLDER = path.join("./uploads");
-
-
-if (!fs.existsSync(UPLOADS_FOLDER)) {
-  fs.mkdirSync(UPLOADS_FOLDER, { recursive: true });
-}
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, UPLOADS_FOLDER);  
-  },
-  filename: (req, file, cb) => {
-    const ext = path.extname(file.originalname); 
-    const uniqueName = Date.now() + "-" + Math.round(Math.random() * 1e9) + ext;
-    cb(null, uniqueName);
+// Configure Cloudinary storage
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'my-project',            // Cloudinary folder name
+    allowed_formats: ['jpg','jpeg','png','webp'],
   }
 });
 
-
-// const fileFilter = (req, file, cb) => {
-//   const allowedTypes = ["image/jpeg", "image/png", "image/jpg", "image/webp"];
-//   if (allowedTypes.includes(file.mimetype)) {
-//     cb(null, true);
-//   } else {
-//     cb(new Error("Only JPG, PNG, WEBP allowed"), false);
-//   }
-// };
-
 export const upload = multer({
   storage,
-  // fileFilter,
   limits: { fileSize: 5 * 1024 * 1024 } // 5MB
 });
