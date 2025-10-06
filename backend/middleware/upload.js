4f6750900fb63746147e7699935a051aa09f28f3
sonukathat <skkhankathat93@gmail.com>
2025-10-06 16:20:27 +0530

    create controler update

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -13,5 +13,5 @@ const storage = new CloudinaryStorage({
 
 export const upload = multer({
   storage,
-  limits: { fileSize: 20 * 1024 * 1024 } // 5MB
+  limits: { fileSize: 20 * 1024 * 1024 }
 });
