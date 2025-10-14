25b11db1959ace90a3ebf65db0af508e74414059
sonukathat <skkhankathat93@gmail.com>
2025-10-14 08:39:21 +0530

    add

--- frontend/src/components/Trending.jsx
+++ frontend/src/components/Trending.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
+import axios from "axios";//asdfghjk
 
 function Trending() {
     const [products, setProducts] = useState([]);
