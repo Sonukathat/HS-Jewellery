d0528061b1d7bca067b8da74c75e6e313ec24b7f
sonukathat <skkhankathat93@gmail.com>
2025-10-16 22:22:55 +0530

    profile log out btn changes

--- frontend/src/components/Userprofile.jsx
+++ frontend/src/components/Userprofile.jsx
@@ -58,7 +58,7 @@ const Userprofile = () => {
 
       <h2 className="text-2xl font-bold text-black mb-2">{user.name}</h2>
       <p className="text-black text-sm opacity-80">{user.email}</p>
-      <button onClick={handleLogout} className="bg-white text-sm rounded-2xl cursor-pointer px-3 py-1 mt-4">
+      <button onClick={handleLogout} className="bg-white text-xs rounded-2xl cursor-pointer px-3 py-1 mt-4">
         Log Out
       </button>
       {error && <p className="text-red-500 mt-2">{error}</p>}
